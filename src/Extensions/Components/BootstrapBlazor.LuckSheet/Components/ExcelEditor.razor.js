544f1cace347fa4e40dd90c38ae5fb926ea6dc5f
Argo-AscioTech <argo@live.ca>
2023-05-18 19:52:35 +0800

    chore: 增加扩展库

---
+++ src/Extensions/Components/BootstrapBlazor.LuckSheet/Components/ExcelEditor.razor.js
@@ -0,0 +1,26 @@
+﻿import { addLink, addScript } from '../../BootstrapBlazor/modules/utility.js'
+import Data from '../../BootstrapBlazor/modules/data.js'
+
+export async function init(id) {
+    await addLink('./_content/BootstrapBlazor.ExcelEditor/css/plugins/pluginsCss.css')
+    await addLink('./_content/BootstrapBlazor.ExcelEditor/css/plugins/plugins.css')
+    await addLink('./_content/BootstrapBlazor.ExcelEditor/iconfont/iconfont.css')
+
+    await addLink('./_content/BootstrapBlazor.ExcelEditor/css/luckysheet.css')
+
+    await addScript('./_content/BootstrapBlazor.ExcelEditor/lib/plugins/plugin.js')
+
+    await addScript('./_content/BootstrapBlazor.ExcelEditor/lib/luckysheet.umd.js')
+
+    var options = {
+        container: id
+    }
+
+    if (luckysheet) {
+        luckysheet.create(options)
+    }
+}
+
+export function dispose(id) {
+
+}
