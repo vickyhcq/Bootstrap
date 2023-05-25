511d4888ff5b3db7ddfd79eaa39d53c6e568326e
Argo-AscioTech <argo@live.ca>
2023-05-25 16:21:35 +0800

    refactor: 更新 Excel 组件资源文件

---
+++ src/Extensions/Components/BootstrapBlazor.Excel/Components/Excel.razor.js
@@ -0,0 +1,22 @@
+﻿import { addLink, addScript } from '../../BootstrapBlazor/modules/utility.js'
+import Data from '../../BootstrapBlazor/modules/data.js'
+
+export async function init(id) {
+    await addLink('./_content/BootstrapBlazor.Excel/css/luckysheet.plugin.css')
+    await addLink('./_content/BootstrapBlazor.Excel/css/luckysheet.css')
+
+    await addScript('./_content/BootstrapBlazor.Excel/js/luckysheet.core.js')
+    await addScript('./_content/BootstrapBlazor.Excel/js/luckysheet.plugin.js')
+
+    var options = {
+        container: id
+    }
+
+    if (luckysheet) {
+        luckysheet.create(options)
+    }
+}
+
+export function dispose(id) {
+
+}
